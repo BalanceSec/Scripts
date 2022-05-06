4735a5ab8bbda122c8946d8cabd121f0a78388da
BalanceSec <104917526+BalanceSec@users.noreply.github.com>
2022-05-06 14:57:55 -0500

    Update passive.sh

--- Passive Recon/passive.sh
+++ Passive Recon/passive.sh
@@ -1,1 +1,12 @@
+#Requires a list of ips titled passive_ips.txt
+#It runs a whois, dig and outputs to the same file. The last part is a reminder to to do more passive recon
 
+#!/bin/bash
+
+hosts=$(cat passive_ips.txt)
+
+for i in $hosts; do whois "$i" > $i.txt; echo -e "\nDig for $i\n" >> $i.txt; dig $i MX >> $i.txt; echo -e "\n" >> $i.txt; done
+
+echo "don't forget to lookup hosts on dnsdumpster and shodan as well. Happy Hacking!"
+
+#whois $host

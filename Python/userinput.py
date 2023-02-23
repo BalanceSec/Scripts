f1cafeaa51a7a8a5372764a0fc2f7269e6e647b9
BalanceSec <104917526+BalanceSec@users.noreply.github.com>
2023-02-23 16:29:41 -0600

    Create userinput.py

---
+++ Python/userinput.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+
+name = input("Enter name: ")
+food = input("What is your favorite food %s?\n" %name)
+print(f"Hello {name}! Have some {food}")
+
+#These inputs are strings and would need to be converted to int is you want to do math with it. 
+    

9e5fa3335101d6727069c40b7a772420e5e9f22a
BalanceSec <104917526+BalanceSec@users.noreply.github.com>
2023-02-23 13:55:05 -0600

    Create math.py

---
+++ Python/math.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+#MATH
+print("MATH")
+print(50 + 50) #add
+print(50 - 50) #subtract
+print(50 * 50) #multiply
+print(50 / 50) #divide (output is float)
+print(50 + 50 - 50 * 50 / 50) #PEMDAS
+print (50 ** 2) #expnonets
+print(50 % 6 ) #modulo
+print(50 / 6) #division with remainder (float)

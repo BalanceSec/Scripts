f2ed57343b7a98dc80001b0b5068adf09a483930
BalanceSec <104917526+BalanceSec@users.noreply.github.com>
2023-02-23 14:14:22 -0600

    Create loops.py

---
+++ Python/loops.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+
+#For loops - start to finish, iterate
+vegetables = ["cucumber", "spinach", "cabbage", "carrots"]
+for x in vegetables:
+	print(x)
+
+#while loops - execute as long true
+i = 1
+
+while i < 10:
+	print(i)
+	i += 1
